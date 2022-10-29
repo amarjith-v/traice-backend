5cef3f0d7813e20e5a4ab7028c08dae0ead2649a
amarjith-v <amarjith.v@bridge-global.com>
2022-10-29 22:11:34 +0530

    new images added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 var imageRouter = require('./routes/image-route');
 const app = express();
+var cors = require('cors')
 const port = 8080;
 app.use(express.json());
 app.use(
@@ -8,6 +9,10 @@ app.use(
         extended: true,
     })
 );
+app.use(express.static('public/images'));
+app.use(cors({
+    origin: 'http://localhost:3000'
+}))
 app.get("/", (req, res) => {
     res.json({ message: "ok" });
 });
